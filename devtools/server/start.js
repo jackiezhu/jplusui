c729e741dbf411682e53e9d25c0d8d710a9fa04c
xuld <xuld@vip.qq.com>
2015-02-12 17:25:06 +0800

    引入开发工具

---
+++ devtools/server/start.js
@@ -0,0 +1,24 @@
+
+var globalConfigs = require("./configs");
+var HttpServer = require("./webserver/httpserver");
+
+var server = module.exports = new HttpServer(globalConfigs);
+
+server.on('start', function(){
+	console.log("[info]Server running at " + this.rootUrl);
+});
+
+server.on('stop', function(){
+	console.log("[info]Server stopped at " + this.rootUrl);
+});
+
+server.on('error', function(){
+	if (e.code == 'EADDRINUSE') {
+		console.error('[Error]Cannot create server on port ' + this.port + (this.address && this.address !== '0.0.0.0' ? ' of ' + this.address : ''));
+	} else {
+		console.error(e);
+	}
+});	
+
+server.start();
+
